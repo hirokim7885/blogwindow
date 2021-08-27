a7f17dc29c306334adc748e8f05beaa8fae3904e
hirokim7885 <hirokim7885@gmail.com>
2021-08-27 10:16:02 +0900

    Userトップ画面レイアウト実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  # before_action :configure_permitted_parameter, if: :devise_controller?
-
-  # def after_sign_out_path_for(resource)
-  #   new_user_session_path
-  # end
-
-  # protected
-  # def configure_permitted_parameter
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :given_name])
-  # end
-
 end
