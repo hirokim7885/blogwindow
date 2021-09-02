6db01456fb96c3a201f371f8994d47fdefbc8288
hirokim7885 <hirokim7885@gmail.com>
2021-09-02 15:37:25 +0900

    前後のblogレコードの取得実装

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -8,4 +8,12 @@ class Blog < ApplicationRecord
   has_many_attached :images
   has_rich_text :content
 
+  def privious
+    user.blogs.order(created_at: :desc, id: :desc).where('created_at <= ? and id < ?', created_at, id).first
+  end
+
+  def next
+    user.blogs.order(created_at: :desc, id: :desc).where('created_at >= ? and id > ?' ,created_at, id).reverse.first
+  end
+
 end
